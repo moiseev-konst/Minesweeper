756a3339f043be5ff420dc31b5c0d6ad852a2bfd
Kostya <moiseev.konst@gmail.com>
2021-03-17 19:04:11 +0300

    new gameClickController

--- src/game.js
+++ src/game.js
@@ -53,9 +53,97 @@ export class Game {
       return false;
     };
   }
+  gameClickController(eventArg) {
+    if (eventArg.buttons == 0) {
+      this.oneButtonCkick(eventArg);
+    } else if (eventArg.buttons == 3) {
+      this.twoButtonsClick(eventArg);
+    }
+  }
+  oneButtonCkick(eventArg) {
+    if (eventArg.button == 0) {
+      this.leftButtonClick(eventArg);
+    } else if (eventArg.button == 2) {
+      this.rightButtonClick(eventArg);
+    }
+  }
+
+  leftButtonClick(eventArg) {
+    if (
+      this.firstClick == undefined &&
+      this.collectionDivMap.indexOf(eventArg.target) >= 0
+    ) {
+      this.leftFirstClick(eventArg);
+    } else {
+      this.leftClick(eventArg);
+    }
+  }
+
+  leftFirstClick(eventArg) {
+    this.firstClick = this.collectionDivMap.indexOf(eventArg.target);
+
+    this.writeMine(this.firstClick, this.quantityMine);
+    this.writeNumberToMap(this.gameMap, this.gameMine);
+    openCellController(this.firstClick);
+  }
+  leftClick(eventArg) {
+    let indexDivClick = this.collectionDivMap.indexOf(eventArg.target);
+    openCellController(indexDivClick);
+  }
+  rightButtonClick(eventArg) {}
+  twoButtonsClick(eventArg) {}
+
+  openCellController(index) {
+    if (this.gameMap[index] > 0) {
+      openCell(index, this.gameMap[index]);
+    } else if (this.gameMap[index] < 0) {
+      openCells(this.openAllMine(), "mine");
+    } else if (this.gameMap[index] == 0) {
+      openCells(this.openSurroundingZero(index));
+    }
+  }
+
+  openCell(index, classCss=null) {
+   
+    this.addClassCssController(this.collectionDivMap[index], classCss);
+  }
+  openCells(arrayIndexes, classCss=null) {
+    arrayIndexes.forEach((index) => {
+      openCell(index, classCss);
+    });
+  }
+  addClassCssController(refDiv, props) {
+    if (Number.isInteger(props)) {
+      let classCss = `n${props < 0 ? 9 : props}`;
+      addClassCss(refDiv, classCss);
+    } else {
+      addClassCss(refDiv, props);
+    }
+  }
+
+  /*
+  gameClickController(eventArg) {
+    if (eventArg.buttons == 0) {
+      if (eventArg.button == 1) {
+        if(this.firstClick == undefined){
+          this.leftFirstClick(eventArg);
+        }else{this.leftClick(eventArg);}
+        
+      } else if (eventArg.button == 2) {
+        this.rightClick(eventArg);
+      }
+    }else if(eventArg.buttons == 3){
+      leftAndRightClick(eventArg)
+    }
+  }
+  leftClick(eventArg) {}
+  rightClick(eventArg) {}
+  leftFirstClick(eventArg){}
+  leftAndRightClick(eventArg){}
+*/
 
   eventClick(eventArg) {
-    console.log(eventArg.button,eventArg.buttons);
+    console.log(eventArg.button, eventArg.buttons);
     if (eventArg.which == 1) {
       if (this.firstClick == undefined) {
         console.log(eventArg);
@@ -121,8 +209,8 @@ export class Game {
         : this.collectionDivMap[indexDivClick].classList.add("flag");
     }
   }
-  addClassCss(div, numberInElement) {
-    div.classList.add(`n${numberInElement < 0 ? 9 : numberInElement}`);
+  addClassCss(div, classCss) {
+    div.classList.add(classCss);
   }
   writeMine(firstClick, quantityMine) {
     while (this.gameMine.length < quantityMine) {
@@ -203,3 +291,26 @@ export class Game {
     return mine;
   }
 }
+/*
+metod (){
+  if{
+    if{
+      if{
+        something_1()
+      } else something_2()
+    }else something_3()
+  }else something_4()
+}
+
+metod(){
+  if{
+    something_1()
+  }something_2()
+}
+something_1(){
+  if{
+    something_3()
+  }something_4()
+}
+  
+}*/
